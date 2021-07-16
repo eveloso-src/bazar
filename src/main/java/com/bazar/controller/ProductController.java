18b9d6f35b39f9e80242d7cecda9ce5a263a6274
Emiliano Veloso <elemudela@gmail.com>
2021-07-15 23:41:46 -0300

    Detalle producto

--- src/main/java/com/bazar/controller/ProductController.java
+++ src/main/java/com/bazar/controller/ProductController.java
@@ -1,13 +1,16 @@
 package com.bazar.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bazar.model.Product;
@@ -31,4 +34,29 @@ public class ProductController {
 		return "index";
 
 	}
+
+	@GetMapping("/product/{id}")
+	public String detailProduct(Model model, @PathVariable String id) {
+		log.debug("detail");
+		Optional<Product> product = productRepository.findById(Integer.valueOf(id));
+		if (product.isPresent()) {
+			model.addAttribute("product", product.get());
+		}
+		model.addAttribute("searchForm", new SearchForm());
+		return "detail";
+
+	}
+
+	@GetMapping("/product1")
+	public String detailProduct1(Model model) {
+		log.debug("detail");
+		String id = "1";
+		Optional<Product> product = productRepository.findById(Integer.valueOf(id));
+		if (product.isPresent()) {
+			model.addAttribute("product", product.get());
+		}
+		return "detail";
+
+	}
+
 }
